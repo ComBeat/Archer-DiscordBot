dc7bc2e6c3aab76f34496883dfe9d6b3ca6f2e84
Yannik Eckert <yannik.eckert@gmx.de>
2020-03-05 22:13:54 +0100

    Added image randomizer

--- archerbot/archerbot.py
+++ archerbot/archerbot.py
@@ -1,6 +1,8 @@
 import json
 import logging
+import os
 from io import FileIO
+from random import randint
 
 import discord
 
@@ -13,7 +15,7 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 # Essential stuff like token and so on
-auth_file = FileIO('../auth.json', mode='r')
+auth_file = FileIO('../bot.json', mode='r')
 json_object = json.load(auth_file)
 bot_token = json_object['bot_token']
 dev_id = int(json_object['dev_id'])
@@ -26,13 +28,13 @@ command_prefix = '>'
 bot_activity = discord.Activity(name='EMIYA', type=discord.ActivityType.listening)
 dev_user = None
 chant = 'I am the bone of my sword\n' \
-      'Steel is my body and fire is my blood\n' \
-      'I have created over a thousand blades\n' \
-      'Unknown to Death,\n' \
-      'Nor known to Life.\n' \
-      'Have withstood pain to create many weapons\n' \
-      'Yet, those hands will never hold anything\n' \
-      'So as I pray, Unlimited Blade Works.'
+        'Steel is my body and fire is my blood\n' \
+        'I have created over a thousand blades\n' \
+        'Unknown to Death,\n' \
+        'Nor known to Life.\n' \
+        'Have withstood pain to create many weapons\n' \
+        'Yet, those hands will never hold anything\n' \
+        'So as I pray, Unlimited Blade Works.'
 
 
 @client.event
@@ -48,29 +50,36 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(command_prefix + 'chant'):
-        await message.channel.send(chant)
+    if message.content.startswith(command_prefix):
+        cmd = message.content[len(command_prefix):]
+        if cmd == 'chant':
+            await message.channel.send(chant)
 
-    elif message.content.startswith(command_prefix + 'rin'):
-        await message.add_reaction('<:rin0:680184904415445045>')
-        await message.add_reaction('\N{THINKING FACE}')
-        await message.channel.send('<:rin0:680184904415445045>')  # Format to post emoji
+        elif cmd == 'rin':
+            await message.channel.send(file=image())
 
-    elif message.content.startswith(command_prefix + 'dm'):
-        await message.author.send('Hello world')
-
-    elif message.content.startswith(command_prefix + 'embed'):
-        embed = discord.Embed(title='Unlimited Blade Works', color=discord.Colour.red())
-        await message.channel.send(embed=embed)
+        elif cmd == 'embed':
+            embed = discord.Embed(title='Unlimited Blade Works', color=discord.Colour.red())
+            await message.channel.send(embed=embed)
 
     # TODO 'emiya' -> go to voice and play EMIYA
 
-    if message.content.startswith(dev_command_prefix + 'ping') and message.author == dev_user:
-        await message.channel.send('You are the bone of my sword')
-        await message.channel.send(dev_command_prefix + 'ping')
-        return
-    else:
-        return
+    elif message.content.startswith(dev_command_prefix) and message.author == dev_user:
+        cmd = message.content[len(dev_command_prefix):]
+        if cmd == 'ping':
+            await message.channel.send('You are the bone of my sword')
+            return
+
+
+# TODO store in different file/class
+def image():
+    dir_path = 'img/'
+    nr_of_items = len(os.listdir(dir_path))
+    # img_nr = randint(0, nr_of_items - 1)
+    img_path = dir_path + 'rin' + str(randint(0, nr_of_items - 1)) + '.png'
+    img = discord.File(img_path)
+
+    return img
 
 
 client.run(bot_token)
